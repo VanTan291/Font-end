839f43f0b1fc803f4c21389a6f3980a4a3ad4682
Lâm Văn Tân <lamvantan@MacBook-Pro-cua-Lam.local>
2022-06-25 22:50:00 +0700

    udpate code

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="mt-0">
+    <div class="">
+     www
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+   
+  },
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const storeData = {
+	modules: {
+	
+	},
+}
+
+const store = new Vuex.Store(storeData)
+export default store
